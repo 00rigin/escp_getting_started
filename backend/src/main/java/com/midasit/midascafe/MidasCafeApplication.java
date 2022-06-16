85a9abbe934f5368e5b8fc8e97fe91a8c2926eea
yhj0122 <whatacg@gmail.com>
2022-06-16 12:02:25 +0900

    PR 수정 내용 - BE 필요없는 import 제거 - RouteButton 생성하여 여러 Link 버튼 만듬
    Login 기능 추가
    * Login/Logout시 state 변화 추가
    * RouteButton 생성하여 여러 Link 버튼 만듬
    * Login 시에만 orderlist 클릭 가능하게 수정

--- backend/src/main/java/com/midasit/midascafe/MidasCafeApplication.java
+++ backend/src/main/java/com/midasit/midascafe/MidasCafeApplication.java
@@ -1,20 +1,7 @@
 package com.midasit.midascafe;
 
-import com.midasit.midascafe.entity.Category;
-import com.midasit.midascafe.entity.Menu;
-import com.midasit.midascafe.entity.Order;
-import com.midasit.midascafe.entity.Status;
-import org.aspectj.weaver.ast.Or;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.util.Date;
 
 @SpringBootApplication
 public class MidasCafeApplication {
