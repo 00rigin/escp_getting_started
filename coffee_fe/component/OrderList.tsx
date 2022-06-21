266f495775ec5107bb43c159dc77b234c381cf5a
yhj0122 <whatacg@gmail.com>
2022-06-21 10:20:14 +0900

    불필요 부분 삭제

--- coffee_fe/component/OrderList.tsx
+++ coffee_fe/component/OrderList.tsx
@@ -1,21 +1,15 @@
 import {useEffect, useState} from "react";
-
 import {DataGrid} from "@mui/x-data-grid";
-
 import {getOrderList} from "../api/apiOrder";
 import {OrderRs} from "../interfaces/rs/orderRs";
 import {orderListColumns, orderListRows} from "../interfaces/constants/Orders";
 
 const OrderList = () => {
 
-
     const [rowData, setRowData] = useState<orderListRows[]>([]);
 
     const getRowData = (orderListData:OrderRs[]) => {
-        // console.log(orderListData);
         const returnData = orderListData.map((item)=> {
-
-            // // console.log(item);
             const coffeeData = {};
             const newData:orderListRows = {id: item.orderID, menuName: item.menuID.menuName, category:item.menuID.category,
                 orderStatus: item.orderStatus, orderDate: item.orderDate};
@@ -23,17 +17,13 @@ const OrderList = () => {
             return newData;
         })
         setRowData(returnData);
-        // console.log(returnData);
-        // return returnData;
     }
 
     useEffect(()=>{
-        console.log("efkjehkfdsa");
         getOrderList()
             .then(res => {
                 getRowData(res);
             })
-
     },[])
 
 
